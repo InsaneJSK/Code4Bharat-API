2c82ed19ec406fb905624aaa001bd63e59f97892
Jaspreet Singh <148172750+InsaneJSK@users.noreply.github.com>
2026-08-14 23:48:29 +0530

    llama-3.3-70b-versatile decomissioned

--- chat_title.py
+++ chat_title.py
@@ -5,7 +5,7 @@ from langchain_core.prompts import ChatPromptTemplate
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 def chat_title(user_input, llm_response):
-    llm = ChatGroq(groq_api_key=GROQ_API_KEY, model_name="llama-3.3-70b-versatile")
+    llm = ChatGroq(groq_api_key=GROQ_API_KEY, model_name="groq/compound")
     CHAT_PROMPT = ChatPromptTemplate.from_template("""You are a helpful assistant that has to give a suitable title to the chat provided between a user and an llm.
     Make sure your title is only 4-5 words.
     User:
